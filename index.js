ab76c43796dd99df9e06bb237c7659397408d88c
Cindy Ha <cindyhaa@gmail.com>
2022-03-27 15:50:38 -0700

    finalize Swiper styling

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 $(document).ready(function() {
-
-  //Flickity for client list
-  // var elem = document.querySelector(".clients-carousel");
-  
-  // var flkty = new Flickity(elem, {
-  //   cellAlign: "left",
-  //   freeScroll: true,
-  //   prevNextButtons: false,
-  //   pageDots: false,
-  //   wrapAround: false,
-  // });
-
   const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
     grabCursor: true,
-    centeredSlides: true,
     slidesPerView: 'auto',
     initialSlide: 0,
+    longSwipes: true,
+    normalizeSlideIndex: true,
   })
-
 });
