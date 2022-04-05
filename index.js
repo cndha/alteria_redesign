220eaac370f23e1df4210a7067580311c68bf6d1
Cindy Ha <cindyhaa@gmail.com>
2022-04-05 14:44:07 -0700

    contact form php file

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ $(document).ready(function() {
     longSwipes: true,
     normalizeSlideIndex: true,
   })
+
+
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      type: 'post',
+      url: 'contact-form.php',
+      data: $('form').serialize(),
+      success: function() {
+        alert("Form was submitted")
+      }
+    });
+  });
+
+  $("#contact-form").ajaxSubmit({url: 'contact-form.php', type: 'post'})
+
 });
