530201a0d77885d62254ee399107783c8160b5f7
Cindy Ha <cindyhaa@gmail.com>
2022-04-05 16:19:44 -0700

    testing with formspree

--- index.js
+++ index.js
@@ -25,4 +25,37 @@ $(document).ready(function() {
 
   // $("#contact-form").ajaxSubmit({url: 'contact-form.php', type: 'post'})
 
+  let form = document.getElementById("contact-form");
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    let status = document.getElementById("submit-status");
+    let data = new FormData(e.target);
+
+    fetch(e.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(res => {
+      if(res.ok) {
+        status.innerHTML = "Thanks for your submission.";
+        form.reset()
+      } else {
+        res.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data['errors'].map(error => error["message"]).join(", ")
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form. Please try again."
+          }
+        })
+      }
+    }).catch(err => {
+      status.innerHTML = "Oops! There was a problem submitting your form. Please try again."
+    })
+  }
+
+  form.addEventListener("submit", handleSubmit);
+
 });
